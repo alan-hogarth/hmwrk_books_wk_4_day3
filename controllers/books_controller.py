58e576d1fbcc8b48b3cfbc8b774e1c066e56f12a
Alan Hogarth <alan.hogarth@outlook.com>
2020-11-25 19:01:14 +0000

    show all books

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -36,4 +36,8 @@ def books():
 
 # DELETE
 # DELETE '/books/<id>'
+@books_blueprint.route("/books/<id>/delete", methods=["POST"])
+def delete_task(id):
+    book_repository.delete(id)
+    return redirect("/books")
 
