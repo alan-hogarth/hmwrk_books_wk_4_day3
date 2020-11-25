8522881372ac084f21540ca9d597a85de01ccccf
Alan Hogarth <alan.hogarth@outlook.com>
2020-11-25 21:37:22 +0000

    extensions

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -15,11 +15,28 @@ def books():
 
 # NEW
 # GET '/books/new'
-
+@books_blueprint.route("/books/new", methods=["GET"])
+def new_book():
+    books = book_repository.select_all()
+    return render_template("books/new.html", new_book=books)
 
 # CREATE
 # POST '/books'
+@books_blueprint.route("/books", methods=["POST"])
+def create_book():
+    title = request.form["title"]
+    genre = request.form["genre"]
+    publisher = request.form["publisher"]
+    author = request.form["author"]
 
+    # select the user using the repository
+    author = author_repository.select(author)
+    # create a new book object
+    book = Book(title, genre, publisher, author)
+    # save that task object back to the db with the save method
+    book_repository.save(book)
+
+    return redirect('/books')
 
 # SHOW
 # GET '/books/<id>'
